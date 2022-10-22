2785e1d3bcc57a26f5af86e51af527635baac42e
Cole von Glahn <cvg117@gmail.com>
2022-10-22 10:37:30 -0500

    now with comments!

--- week_04/homework.js
+++ week_04/homework.js
@@ -1,50 +1,51 @@
 /* D3 Line Chart */
 
-const height = 500,
+const height = 500, // Define height, width and margin buffers for chart
     width = 800,
     margin = ({ top: 15, right: 30, bottom: 35, left: 40 });
     
-const svg = d3.select("#chart")
+const svg = d3.select("#chart") // resizes chart based on window
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
 d3.csv('long-term-interest-canada.csv').then(data => {
     
-    let timeParse = d3.timeParse("%Y-%m")
+    let timeParse = d3.timeParse("%Y-%m") // Parse time data from year-month
 
+    // Time parse and number coercion
     for (let d of data) {
         d.Num = +d.Num
         d.Month = timeParse(d.Month);
     }
 
-    let x = d3.scaleTime()
+    let x = d3.scaleTime() // Provides data, positioning, and dimensions for elements on x-axis
         .domain(d3.extent(data, d => d.Month))
         .range([margin.left, width - margin.right]);
 
-    let y = d3.scaleLinear()
+    let y = d3.scaleLinear() // Provides data and dimensions for elements on y-axis
         .domain([0,d3.max(data, d => d.Num)])
         .range([height - margin.bottom, margin.top]);
 
-    svg.append("g")
+    svg.append("g") // Y-axis formatting and position
         .attr("transform", `translate(${margin.left},0)`)
         .attr("class", "y-axis") //define as class to apply certain css formatting on a class basis 
         .call(d3.axisLeft(y).tickSizeOuter(0).tickFormat(d => d + "%").tickSize(-width));
 
-    svg.append("g")
+    svg.append("g") // X-axis formatting and position
       .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
-    svg.append("text")
+    svg.append("text") // Label the x-axis
       .attr("class", "x-label")
       .attr("text-anchor", "end")
       .attr("x", width - margin.right)
       .attr("y", height)
-      .attr("dx", "0.5em") //shifting attribute
-      .attr("dy", "-.01em") //shifting attribute
+      .attr("dx", "0.5em") 
+      .attr("dy", "-.01em") 
       .style("font-size", "12px")
       .text("Month");
     
-    svg.append("text")
+    svg.append("text") // Label the y-axis
       .attr("class", "y-label")
       .attr("text-anchor", "end")
       .attr("x", -margin.top/2)
